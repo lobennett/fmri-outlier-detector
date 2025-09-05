4a85490a258e0f09edf27728bd2a1a6a7922a99c
Logan Bennett <bennett.j.logan@gmail.com>
2025-09-05 15:35:47 -0700

    change memory and cpu request

--- run_network.sh
+++ run_network.sh
@@ -5,14 +5,14 @@
 #SBATCH --time=1-00:00:00
 #SBATCH -p russpold,hns,normal
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=6G
 
 # Run using uv and specify the venv directory directly
 current_dir="$(pwd)"
 network_script="/home/users/logben/fmri-outlier-detector/run_network.py"
 base_dir="/scratch/users/logben/poldrack_glm/level1/output"
-output_dir="/scratch/users/logben/fmriprep_outlier_check_20250903"
+output_dir="/scratch/users/logben/fmriprep_outlier_check_20250905"
 
 echo "Running analysis to create figures..."
 uv run --directory $current_dir python $network_script \
